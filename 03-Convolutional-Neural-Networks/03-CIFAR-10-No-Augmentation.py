74c255dd824300b8e6a6adf565fc86eb11af074f
rodrigo <rodrigotdk@gmail.com>
2020-09-02 21:39:03 -0300

    Add examples of convolutional neural networks

---
+++ 03-Convolutional-Neural-Networks/03-CIFAR-10-No-Augmentation.py
@@ -0,0 +1,171 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep  2 20:34:58 2020
+
+@author: rodrigo
+"""
+
+import itertools
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import (Input, Conv2D,
+                                     Dense, Flatten,
+                                     Dropout, GlobalMaxPooling2D,
+                                     MaxPooling2D, BatchNormalization)
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
+
+
+# Load the Data
+data = tf.keras.datasets.cifar10
+(X_train, y_train), (X_test, y_test) = data.load_data()
+
+# Classes in Fashion-MNIST
+class_names = ['airplane',
+               'automobile',
+               'bird',
+               'cat',
+               'deer',
+               'dog',
+               'frog',
+               'horse',
+               'ship',
+               'truck']
+
+k = len(class_names)
+
+print("X_train shape: {}".format(X_train.shape))
+print("X_test shape: {}".format(X_test.shape))
+print("y_train shape: {}".format(y_train.shape))
+print("y_test shape: {}".format(y_test.shape))
+
+
+# Preprocessing
+# The pixel values ​​are between 0 and 255. We scaled these values ​​in the
+# range of 0 and 1 before before feeding the neural network model. To do this,
+# we divide the values ​​by 255.
+
+# plt.figure()
+# plt.imshow(X_train[0])
+# plt.colorbar()
+# plt.grid(False)
+# plt.show()
+
+X_train, X_test = X_train/255.0, X_test/255.0
+
+# Flatten the labels array
+y_train, y_test = y_train.flatten(), y_test.flatten()
+
+# plt.figure(figsize=(10,10))
+# for i in range(25):
+#     plt.subplot(5,5,i+1)
+#     plt.xticks([])
+#     plt.yticks([])
+#     plt.grid(False)
+#     plt.imshow(X_train[i], cmap=plt.cm.binary)
+#     plt.xlabel(class_names[y_train[i]])
+# plt.show()
+
+print("X_train shape: {}".format(X_train.shape))
+print("X_test shape: {}".format(X_test.shape))
+
+# Build the Model
+i = Input(shape=X_train[0].shape)
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(i)
+x = BatchNormalization()(x)
+x = Conv2D(32, (3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPooling2D((2, 2))(x)
+x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPooling2D((2, 2))(x)
+x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = Conv2D(128, (3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+# x = MaxPooling2D((2, 2))(x)
+x = GlobalMaxPooling2D()(x)
+x = Flatten()(x)
+x = Dropout(0.2)(x)
+x = Dense(1024, activation='relu')(x)
+x = Dropout(0.2)(x)
+x = Dense(k, activation='softmax')(x)
+
+model = Model(i, x)
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Fit the model
+r = model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=50)
+
+# Plot loss per iteration
+plt.plot(r.history['loss'], label='loss')
+plt.plot(r.history['val_loss'], label='val_loss')
+plt.legend(loc='best')
+plt.show()
+
+# Plot accuracy per iteration
+plt.plot(r.history['accuracy'], label='acc')
+plt.plot(r.history['val_accuracy'], label='val_acc')
+plt.legend(loc='best')
+plt.show()
+
+# Confusion Matrix
+p_test = model.predict(X_test).argmax(axis=1)  # Get the highest probability
+cm = confusion_matrix(y_test, p_test)
+plot_confusion_matrix(cm, list(range(10)))
+
+# Show some misclassified examples
+misclassified_idx = np.where(p_test != y_test)[0]
+i = np.random.choice(misclassified_idx)
+plt.imshow(X_test[i], cmap='gray')
+plt.title("True label: {0} Predicted: {1}".format(class_names[y_test[i]],
+                                                  class_names[p_test[i]]))
+
+# Summarize the model
+model.summary()
